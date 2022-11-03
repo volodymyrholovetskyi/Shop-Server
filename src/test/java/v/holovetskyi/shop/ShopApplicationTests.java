3d7115626ca75d7b566941f3b71f67f2a1583b83
Volodymyr Holovetskyi <volodymyrgolovetskyi1995@gmail.com>
2022-11-03 21:22:01 +0100

    inital commit

---
+++ src/test/java/v/holovetskyi/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package v.holovetskyi.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
