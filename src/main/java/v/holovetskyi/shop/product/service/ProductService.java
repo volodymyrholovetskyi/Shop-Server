a93f9896645ec322458afb3355d759e0414cc04a
Volodymyr Holovetskyi <volodymyrgolovetskyi1995@gmail.com>
2022-11-06 17:30:58 +0100

    product pagination

--- src/main/java/v/holovetskyi/shop/product/service/ProductService.java
+++ src/main/java/v/holovetskyi/shop/product/service/ProductService.java
@@ -1,11 +1,11 @@
 package v.holovetskyi.shop.product.service;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import v.holovetskyi.shop.product.model.Product;
 import v.holovetskyi.shop.product.repository.ProductRepository;
-
-import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -13,7 +13,7 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public List<Product> getProducts(){
-        return productRepository.findAll();
+    public Page<Product> getProducts(Pageable pageable) {
+        return productRepository.findAll(pageable);
     }
 }
--- src/main/java/v/holovetskyi/shop/product/web/ProductController.java
+++ src/main/java/v/holovetskyi/shop/product/web/ProductController.java
@@ -1,13 +1,16 @@
 package v.holovetskyi.shop.product.web;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import v.holovetskyi.shop.product.model.Product;
 import v.holovetskyi.shop.product.service.ProductService;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
@@ -18,7 +21,7 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping
-    List<Product> getProducts(){
-        return productService.getProducts();
+    Page<Product> getProducts(Pageable pageable){
+        return productService.getProducts(pageable);
     }
 }
