0a405f72c3ca3d999a5f8989213f3e0cc9b7a02d
Volodymyr Holovetskyi <volodymyrgolovetskyi1995@gmail.com>
2022-11-13 19:20:41 +0100

    Delete product

--- src/main/java/v/holovetskyi/shop/admin/web/AdminProductController.java
+++ src/main/java/v/holovetskyi/shop/admin/web/AdminProductController.java
@@ -38,6 +38,12 @@ public class AdminProductController {
         return productService.updateProduct(mapAdminProduct(productDTO, id));
     }
 
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id){
+        productService.deleteProduct(id);
+    }
+
+
     private AdminProduct mapAdminProduct(AdminProductDTO productDTO, Long id) {
         return AdminProduct.builder()
                 .id(id)
